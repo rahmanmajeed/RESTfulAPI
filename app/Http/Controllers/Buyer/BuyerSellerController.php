05bf3d8064c0ff91434afffa6a4d2efc15b48e73
Majeed <rahmanmajeed46@gmail.com>
2018-11-08 22:29:19 +0600

    implement complex operations for buyer

---
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerSellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $sellers = $buyer->transactions()->with('product.seller')->get()->pluck('product.seller')->unique('id')->values();
+        return $this->showAll($sellers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Buyer $buyer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Buyer $buyer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Buyer $buyer)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 /** Buyers */
 Route::resource('buyers','Buyer\BuyerController',['only' => ['index','show']]);
 Route::resource('buyers.transactions','Buyer\BuyerTransactionController',['only' => ['index']]);
+Route::resource('buyers.products','Buyer\BuyerProductController',['only' => ['index']]);
+Route::resource('buyers.sellers','Buyer\BuyerSellerController',['only' => ['index']]);
+Route::resource('buyers.categories','Buyer\BuyerCategoryController',['only' => ['index']]);
 
 /** Categories */
 
